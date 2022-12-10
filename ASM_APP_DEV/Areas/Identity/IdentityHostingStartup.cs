4d9bca5f0f46b015b719f23262cd9b3eaf9dbcb5
hieu36075 <hieu36075@gmail.com>
2022-12-10 14:27:05 +0700

    Scaffold register and login

---
+++ ASM_APP_DEV/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ASM_APP_DEV.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASM_APP_DEV.Areas.Identity.IdentityHostingStartup))]
+namespace ASM_APP_DEV.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
