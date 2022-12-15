c69bac0ac642d2e91939f9bc68d45403401af031
hieu36075 <hieu36075@gmail.com>
2022-12-15 20:03:30 +0700

    Add OrderController

---
+++ ASM_APP_DEV/Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using ASM_APP_DEV.Data;
+using ASM_APP_DEV.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASM_APP_DEV.Controllers
+{
+    public class BooksController : Controller
+    {
+        private ApplicationDbContext _context;
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //var booksInDb = _dbContext.Books.ToList();
+            IEnumerable<Book> booksInDb = _context.Books.ToList();
+
+            return View(booksInDb);
+        }
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            //var booksInDb = _dbContext.Books.ToList();
+            var bookInDb = _context.Books.SingleOrDefault(b => b.Id == id);
+
+            return View(bookInDb);
+        }
+
+    }
+}
---
+++ ASM_APP_DEV/Controllers/OrdersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASM_APP_DEV.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+    }
+}
