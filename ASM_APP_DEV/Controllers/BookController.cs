98e24ddfd11cfcd76def45a7bbcdb06693e62cd1
Hoang Bui <hoangbnmgcd191187@fpt.edu.vn>
2022-12-21 20:39:23 +0700

    fix conflic

---
+++ ASM_APP_DEV/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using ASM_APP_DEV.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ASM_APP_DEV.Controllers
+{
+    public class BookController : Controller
+    {
+        ApplicationDbContext dbContext;
+
+        public BookController(ApplicationDbContext context)
+        {
+            this.dbContext = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var booksInDb = dbContext.Books.ToList();
+            return View(booksInDb);
+        }
+    }
+}
---
+++ ASM_APP_DEV/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using ASM_APP_DEV.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ASM_APP_DEV.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        ApplicationDbContext dbContext;
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var categoriesInDb = dbContext.Categories.ToList();
+            
+            return View(categoriesInDb); 
+        }
+    }
+}
